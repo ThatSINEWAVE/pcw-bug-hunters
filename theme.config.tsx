a0e6d51cb6ac4bb67c7439e7329ea1dd981405b2
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-09-22 09:15:45 +0300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,16 +2,16 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>PCW Bug Hunters</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/ThatSINEWAVE',
   },
   chat: {
-    link: 'https://discord.com',
+    link: 'https://discord.gg/2nHHHBWNDw',
   },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: 'https://github.com/ThatSINEWAVE',
   footer: {
-    text: 'Nextra Docs Template',
+    text: 'Made by SINEWAVE',
   },
 }
 
