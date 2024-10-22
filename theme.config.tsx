95dfa976c987833082006ac5ef6e6fa19d5b51fa
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-10-22 17:58:01 +0300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -17,20 +17,13 @@ const config: DocsThemeConfig = {
   },
 
   // Add the head function here for custom meta tags
-  head() {
-    const { asPath, defaultLocale, locale } = useRouter();
-    const { frontMatter } = useConfig();
-
-    const url = `https://pcw-bughunters.vercel.app${defaultLocale === locale ? asPath : `/${locale}${asPath}`}`;
-
-    return (
-      <>
-        <meta property="og:url" content={url} />
-        <meta property="og:title" content={frontMatter.title || 'PCW Bug Hunters'} />
-        <meta property="og:description" content={frontMatter.description || 'Work in Progress'} />
-      </>
-    );
-  },
-};
+  head: (
+    <>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta property="og:title" content="PCW Bug Hunters" />
+      <meta property="og:description" content="Work in Progress" />
+    </>
+  )
+}
 
 export default config;
