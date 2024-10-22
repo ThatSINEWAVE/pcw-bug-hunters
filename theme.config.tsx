4e4abc6043475c888df3c9e05eb396dfc389bb29
David <133239148+ThatSINEWAVE@users.noreply.github.com>
2024-10-22 17:43:33 +0300

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -16,3 +16,22 @@ const config: DocsThemeConfig = {
 }
 
 export default config
+  head() {
+    const { asPath, defaultLocale, locale } = useRouter()
+    const { frontMatter } = useConfig()
+    const url =
+      'https://my-app.com' +
+      (defaultLocale === locale ? asPath : `/${locale}${asPath}`)
+
+    return (
+      <>
+        <meta property="og:url" content={url} />
+        <meta property="og:title" content={frontMatter.title || 'PCW Bug Hunters'} />
+        <meta
+          property="og:description"
+          content={frontMatter.description || 'Work in Progress'}
+        />
+      </>
+    )
+  }
+}
